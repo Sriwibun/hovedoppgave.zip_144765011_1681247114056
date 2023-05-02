c10a3c1f3743932ce6071e78ff92d0bbb46a0a96
Sriwibun <sriwiw20@gmail.com>
2023-05-02 13:52:47 +0100

    commit

---
+++ Paint/script/drawingoObjects.js
@@ -0,0 +1,9 @@
+export function TdrawingObject(aCTX , aObjectType, aStrokeColor, aFillColor, aStrokeSize){
+    const ctx = aCTX;
+    const points = [];
+
+    this.draw = function(){
+
+    }
+
+}
